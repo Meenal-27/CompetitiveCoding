f3e65d00f6292ce0ce2ac5f02899b3c9710e6456
Meenal <meenal.gupta727@gmail.com>
2021-06-11 12:30:17 +0530

    Newfile

---
+++ interviewbit/2_easy_oopcmpl.cpp
@@ -0,0 +1,9 @@
+ 
+//  int a = 0, b = 0;    
+//         for (i = 0; i < N; i++) {
+//             a = a + rand();  
+//         }
+//         for (j = 0; j < M; j++) {
+//             b = b + rand();
+//         }
+//          O(N + M) time, O(1) space
